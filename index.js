09efed8a85272edf1c77fd1f162f136661ca9090
tebiikun <esteban.ciccarelli@gmail.com>
2017-05-18 21:39:46 -0300

    pide usuario de inicio y muestra quien escribe

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-	socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+	socket.on('chat message', function(msg, user){
+    io.emit('chat message', user + ':' + msg);
   });
 });
 
